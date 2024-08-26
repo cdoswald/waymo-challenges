4a01625342672e3a1a967b920870e785988c6b3d
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2024-08-25 22:36:51 -0400

    added waymo-provided utility functions

--- 3DSemanticSeg/src/utils/utils_waymo.py
+++ 3DSemanticSeg/src/utils/utils_waymo.py
@@ -1,18 +1,18 @@
 """Waymo-provided utility functions for Waymo Open Dataset challenges.
 
 Note that all functions in this module are originally provided by Waymo
-and used without modification (e.g., functions provided in tutorials in the 
-waymo-open-dataset GitHub repository). 
-
-Functions that are based on Waymo utility functions but that have been modified 
-will be stored in utils.py (with source info).
+and used without modification. Functions that are based on Waymo utility 
+functions but that have been modified will be stored in utils.py 
+(with source info).
 """
 
 import os
 from typing import Dict, List, Optional, Tuple
 import warnings
+import zlib
 
 import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow
 import tensorflow.compat.v1 as tf
 
@@ -20,7 +20,7 @@ tf.enable_eager_execution()
 
 from waymo_open_dataset import dataset_pb2 as wod
 
-
+## Source: https://github.com/waymo-research/waymo-open-dataset/blob/master/tutorial/tutorial_3d_semseg.ipynb
 def convert_range_image_to_point_cloud_labels(
     frame, range_images, segmentation_labels, ri_index=0
 ):
@@ -57,3 +57,72 @@ def convert_range_image_to_point_cloud_labels(
 
         point_labels.append(sl_points_tensor.numpy())
     return point_labels
+
+
+## Source: https://github.com/waymo-research/waymo-open-dataset/blob/master/tutorial/tutorial_3d_semseg.ipynb
+def get_range_image_point_indexing(range_images, ri_index=0):
+  """Get the indices of the valid points (of the TOP lidar) in the range image.
+
+  The order of the points match those from convert_range_image_to_point_cloud
+  and convert_range_image_to_point_cloud_labels.
+
+  Args:
+    range_images: A dict of {laser_name, [range_image_first_return,
+       range_image_second_return]}.
+    ri_index: 0 for the first return, 1 for the second return.
+
+  Returns:
+    points_indexing_top: (N, 2) col and row indices of the points in the
+      TOP lidar.
+  """
+  points_indexing_top = None
+  xgrid, ygrid = np.meshgrid(range(TOP_LIDAR_COL_NUM), range(TOP_LIDAR_ROW_NUM))
+  col_row_inds_top = np.stack([xgrid, ygrid], axis=-1)
+  range_image = range_images[open_dataset.LaserName.TOP][ri_index]
+  range_image_tensor = tf.reshape(
+      tf.convert_to_tensor(range_image.data), range_image.shape.dims)
+  range_image_mask = range_image_tensor[..., 0] > 0
+  points_indexing_top = col_row_inds_top[np.where(range_image_mask)]
+  return points_indexing_top
+
+
+## Source: https://github.com/waymo-research/waymo-open-dataset/blob/master/tutorial/tutorial_3d_semseg.ipynb
+def compress_array(array: np.ndarray, is_int32: bool = False):
+  """Compress a numpy array to ZLIP compressed serialized MatrixFloat/Int32.
+
+  Args:
+    array: A numpy array.
+    is_int32: If true, use MatrixInt32, otherwise use MatrixFloat.
+
+  Returns:
+    The compressed bytes.
+  """
+  if is_int32:
+    m = open_dataset.MatrixInt32()
+  else:
+    m = open_dataset.MatrixFloat()
+  m.shape.dims.extend(list(array.shape))
+  m.data.extend(array.reshape([-1]).tolist())
+  return zlib.compress(m.SerializeToString())
+
+
+## Source: https://github.com/waymo-research/waymo-open-dataset/blob/master/tutorial/tutorial_3d_semseg.ipynb
+def decompress_array(array_compressed: bytes, is_int32: bool = False):
+  """Decompress bytes (of serialized MatrixFloat/Int32) to a numpy array.
+
+  Args:
+    array_compressed: bytes.
+    is_int32: If true, use MatrixInt32, otherwise use MatrixFloat.
+
+  Returns:
+    The decompressed numpy array.
+  """
+  decompressed = zlib.decompress(array_compressed)
+  if is_int32:
+    m = open_dataset.MatrixInt32()
+    dtype = np.int32
+  else:
+    m = open_dataset.MatrixFloat()
+    dtype = np.float32
+  m.ParseFromString(decompressed)
+  return np.array(m.data, dtype=dtype).reshape(m.shape.dims)
